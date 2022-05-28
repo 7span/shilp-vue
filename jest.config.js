bf11c609863be33e862478a82eae141c45025619
jay-p-b-7span <jay.p.b@7span.com>
2022-05-28 21:46:21 +0530

    Fixed Jest config issue

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,13 @@ module.exports = {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.js$": "babel-jest",
   },
-
-  transformIgnorePatterns: ["/node_modules/"],
+  // below value is taken from stack overflow to fix unexpected token issue
+  // https://stackoverflow.com/questions/49263429/jest-gives-an-error-syntaxerror-unexpected-token-export#:~:text=%22transformIgnorePatterns%22%3A%20%5B%0A%20%20%20%20%20%20%22node_modules/(%3F!%40ngrx%7C(%3F!deck.gl)%7Cng%2Ddynamic)%22%0A%20%20%20%20%5D
+  // defualt config provides
+  // transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
 
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
@@ -28,4 +31,5 @@ module.exports = {
   ],
 
   preset: "@vue/cli-plugin-unit-jest",
+  collectCoverage: false,
 };
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Button from "@/components/Button.vue";
 
 describe("Button.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "Log In";
-    const wrapper = shallowMount(Button, {
+    const wrapper = mount(Button, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
